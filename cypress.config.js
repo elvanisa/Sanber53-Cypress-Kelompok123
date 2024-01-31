939d6dd0e1185e5d8209633f47e41040250547c5
elvanisa <elvanisamuhsina@gmail.com>
2024-01-31 14:09:58 +0700

    update POM

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,16 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "https://www.saucedemo.com/",
+    env: {
+      username: "elva"
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
       
     },
+    defaultCommandTimeout: 5432,
+    viewportWidth: 990,
+    viewportHeight: 650
   },
 });
--- cypress/e2e/3-web/saucedemo/login.cy.js
+++ cypress/e2e/3-web/saucedemo/login.cy.js
@@ -1,17 +1,21 @@
+import loginPage from "../../../support/pageObject/loginPage"
+
 describe('Login Functionality', () => {
   beforeEach(() => {
-    cy.visit('https://www.saucedemo.com/')
+    cy.visit('')
   })
-  it('Verify Failed Login - Wrong Creds', () => {
-    cy.get('#user-name').clear().type('elvanisa')
-    cy.get('[name="password"]').clear().type('123456')
-    cy.get('.submit-button.btn_action').click()
-    cy.get('[data-test="error"]').should('be.visible')
+  it.only('Verify Failed Login - Wrong Creds', () => {
+    cy.get(loginPage.usr).type('elvanisa')
+    cy.get(loginPage.pwd).type('123456')
+    //cy.get('.submit-button.btn_action').click()
+    loginPage.clickLogin()
+    //cy.get('[data-test="error"]').should('be.visible')
+    loginPage.verifyError()
   })
-  it.only('Verify Failed Login - Locked User', () => {
-    cy.get('#user-name').type('locked_out_user')
-    cy.get('[name="password"]').type('secret_sauce')
-    cy.get('.submit-button.btn_action').click()
-    cy.get('[data-test="error"]').should('contain.text','Sorry, this user has been locked out')
+  it('Verify Failed Login - Locked User', () => {
+    cy.get(loginPage.usr).type('locked_out_user')
+    cy.get(loginPage.pwd).type('secret_sauce')
+    cy.get(loginPage.login_btn).click()
+    cy.get(loginPage.err).should('contain.text','Sorry, this user has been locked out')
   })
 })
