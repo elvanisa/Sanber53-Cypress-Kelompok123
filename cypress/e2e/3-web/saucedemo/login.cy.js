3fe31f0809f43e60d777733c2a6be7e4bed748cb
elvanisa <elvanisamuhsina@gmail.com>
2024-01-31 13:36:25 +0700

    Login Test Cases

--- cypress/e2e/3-web/saucedemo/login.cy.js
+++ cypress/e2e/3-web/saucedemo/login.cy.js
@@ -1,5 +1,17 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Login Functionality', () => {
+  beforeEach(() => {
+    cy.visit('https://www.saucedemo.com/')
+  })
+  it('Verify Failed Login - Wrong Creds', () => {
+    cy.get('#user-name').clear().type('elvanisa')
+    cy.get('[name="password"]').clear().type('123456')
+    cy.get('.submit-button.btn_action').click()
+    cy.get('[data-test="error"]').should('be.visible')
+  })
+  it.only('Verify Failed Login - Locked User', () => {
+    cy.get('#user-name').type('locked_out_user')
+    cy.get('[name="password"]').type('secret_sauce')
+    cy.get('.submit-button.btn_action').click()
+    cy.get('[data-test="error"]').should('contain.text','Sorry, this user has been locked out')
   })
 })
